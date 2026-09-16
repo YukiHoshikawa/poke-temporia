300d98151de76550c1379e4c49fe27b4bb35a340
butterfly6186 <329761920+butterfly6186@users.noreply.github.com>
2026-09-15 19:02:40 -0500

    fixed shit

--- src/new_game.c
+++ src/new_game.c
@@ -190,7 +190,7 @@ void NewGameInitData(void)
     ResetGabbyAndTy();
     ClearSecretBases();
     ClearBerryTrees();
-    SetMoney(&gSaveBlock1Ptr->money, 3000);
+    SetMoney(&gSaveBlock1Ptr->money, 2999);
     SetCoins(0);
     GetNewDerby();
     ResetLinkContestBoolean();
